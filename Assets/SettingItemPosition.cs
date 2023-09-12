b2df36af486f21028379fbbdb8cfa44dd2701021
popovon0325@gmail.com <popovon0325@gmail.com>
2023-09-13 00:30:54 +0900

    コミット

--- Assets/SettingItemPosition.cs
+++ Assets/SettingItemPosition.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class SettingItemPosition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform m_gamePlayer;
+    private Transform[] childrenTrans = null;
+    private void Start()
     {
+        var myT = transform;
+        childrenTrans = new Transform[myT.childCount];
         
+        for (int i = 0; i < childrenTrans.Length; i++)
+            childrenTrans[i] = myT.GetChild(i);
     }
-
-    // Update is called once per frame
-    void Update()
+    private async void Update()
     {
-        
+        for (int i = 0; i < 8; i++)
+            await UniTask.Yield();
+
+        Vector2 playerPosition = new Vector2(m_gamePlayer.position.x, m_gamePlayer.position.z);
+        for (int i = 0; i < childrenTrans.Length; i++)
+        {
+            var child = childrenTrans[i];
+            child.position = new Vector3(playerPosition.x, child.position.y, playerPosition.y);
+        }
     }
 }
